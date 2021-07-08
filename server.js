441660195fff0cb53e37b91e03ff2c71326dfb99
Skipper Strange <skipperstrange@gmail.com>
2021-07-08 23:37:19 +0000

    package.json recovered

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const cors = require("cors"),
   app = express();
 app.use(cors());
 app.use(serveStatic(path.join(__dirname, "dist")));
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 808;
 app.listen(port);
 console.log(`app is listening on port: ${port}`);
